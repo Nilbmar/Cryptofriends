323d1dc9d454b5f70fb8c51e350cb27822537df1
Nilbmar <Nilbmar@gmail.com>
2018-04-20 18:35:05 -0400

    Cleaned up comments

--- Cryptograms/src/Cryptofriends/Main.java
+++ Cryptograms/src/Cryptofriends/Main.java
@@ -4,8 +4,6 @@ import core.Managers.GameManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -28,16 +26,6 @@ public class Main extends Application {
 			controller.setGameManager(gameMan);
 			controller.createBoard();
 			controller.loadNewPuzzle();
-			
-			/*
-			String phraseToTest = "I'm in a house full of baboons." 
-					+ " They're driving me crazy! Help me! I'll pay you."
-					+ "Like $4000? I'm in a house full of baboons." 
-					+ " They're driving me crazy! Help me! I'll pay you."
-					+ "Like $4000?";
-			controller.setupPuzzle(gameMan.getPuzzle(phraseToTest));
-			*/
-			
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
