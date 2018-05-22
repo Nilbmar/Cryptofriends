e56522154dd7a2b4230c4d1af4821a4bc683d46e
Nilbmar <Nilbmar@gmail.com>
2018-05-22 16:28:03 -0400

    Updated for when swapping to new puzzle

--- Cryptograms/src/core/Data/PlayerScore.java
+++ Cryptograms/src/core/Data/PlayerScore.java
@@ -2,22 +2,33 @@ package core.Data;
 
 public class PlayerScore {
 	private String playerKey = null;
-	private float score = 100;
+	private float puzzleScore = 100;
+	private float totalScore = 0;
 	
 	public PlayerScore(String playerKey) {
 		this.playerKey = playerKey;
 	}
 	
-	public float getScore() { return score; }
+	public String getPlayerKey() { return playerKey; }
+	
+	public float getScore() { return puzzleScore; }
+	public float getTotalScore() { return totalScore; }
 	
 	public void reduceScore(float percent) {
-		score -= percent;
+		puzzleScore -= percent;
 		
-		if (score < 0) {
-			score = 0;
+		if (puzzleScore < 0) {
+			puzzleScore = 0;
 		}
 	}
 	
-	public void scoreZero() { score = 0; }
+	// Player has used "Answer Puzzle" Reveal
+	public void scoreZero() { puzzleScore = 0; }
+	
+	// Update and set values for next puzzle
+	public void changedPuzzle() { 
+		totalScore += puzzleScore;
+		puzzleScore = 100;
+	}
 
 }
